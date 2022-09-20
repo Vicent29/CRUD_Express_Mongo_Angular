224026f111fdec3053327752eea4a6460c806d5f
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2022-09-21 01:04:19 +0200

    Add-Product funcionando correctamente

--- client/src/app/components/add-product/add-product.component.ts
+++ client/src/app/components/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-add-product',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    prod_nom: '',
+    id_prod_typ: '',
+    prod_desc: '',
+    precio: '',
+    id_prod_cat: ''
+  };
+  submitted = false;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+  }
+
+  saveProduct(): void {
+    const data = {
+      prod_nom: this.product.prod_nom,
+      id_prod_typ: this.product.id_prod_typ,
+      prod_desc: this.product.prod_desc,
+      precio: this.product.precio,
+      id_prod_cat: this.product.id_prod_cat
+    };
+
+    this.productService.create(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.submitted = true;
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  newProduct(): void {
+    this.submitted = false;
+    this.product = {
+      prod_nom: '',
+      id_prod_typ: '',
+      prod_desc: '',
+      precio: '',
+      id_prod_cat: ''
+    };
   }
 
 }
