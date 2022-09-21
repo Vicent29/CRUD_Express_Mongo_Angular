ceaf63643ba9a7e6fb4f611989d191173d87f65e
santi <santimartinezalbert02@gmail.com>
2022-09-21 21:42:35 +0200

    start category routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+// import { CategoryListComponent } from './components/category-list/category-list.component';
+// import { CategoryDetailsComponent } from './components/category-details/category-details.component';
+// import { AddCategoryComponent } from './components/add-category/add-category.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'product', pathMatch: 'full' },
   { path: 'product', component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
-  { path: 'add', component: AddProductComponent }
+  { path: 'add_prod', component: AddProductComponent },
+  // { path: 'add_category', component: AddCategoryComponent },
+  // { path: 'category', component: CategoryListComponent },
+  // { path: 'category/:id', component: CategoryDetailsComponent }
+
+
 ];
 
 @NgModule({
---
+++ client/src/app/models/category.model.ts
@@ -0,0 +1,5 @@
+export class Category {
+    id?: any;
+     id_cat?: String;
+      cat_name?: String;
+}
