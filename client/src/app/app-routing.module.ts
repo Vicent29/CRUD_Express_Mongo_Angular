baf4f0e570a13f3e2db379f6bcd132426f6c9799
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2022-09-26 12:21:28 +0200

    lazy_load categories finished

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { AddProductComponent } from './components/add-product/add-product.component';
-import { CategoryListComponent } from './components/category-list/category-list.component';
-import { CategoryDetailsComponent } from './components/category-details/category-details.component';
-import { AddCategoryComponent } from './components/add-category/add-category.component';
+// import { ProductListComponent } from './components/product-list/product-list.component';
+// import { ProductDetailsComponent } from './components/product-details/product-details.component';
+// import { AddProductComponent } from './components/add-product/add-product.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'product', pathMatch: 'full' },
-  { path: 'product', component: ProductListComponent },
-  { path: 'product/:id', component: ProductDetailsComponent },
-  { path: 'add_prod', component: AddProductComponent },
-  { path: 'add_cat', component: AddCategoryComponent },
-  { path: 'category', component: CategoryListComponent },
-  { path: 'category/:id', component: CategoryDetailsComponent }
+  {
+    path: 'category',
+    loadChildren: () =>
+      import('./category.module').then((m) => m.CategoryModule),
+  },
+  // {
+  //   path: 'product',
+  //   loadChildren: () =>
+  //     import('./product.module').then((m) => m.ProductsModule),
+  // },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddProductComponent } from './components/add-product/add-product.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { AddCategoryComponent } from './components/add-category/add-category.component';
-import { CategoryDetailsComponent } from './components/category-details/category-details.component';
-import { CategoryListComponent } from './components/category-list/category-list.component';
+// import { AddProductComponent } from './components/add-product/add-product.component';
+// import { ProductDetailsComponent } from './components/product-details/product-details.component';
+// import { ProductListComponent } from './components/product-list/product-list.component';
 
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,12 +16,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent,
-    ProductDetailsComponent,
-    ProductListComponent,
-    AddCategoryComponent,
-    CategoryDetailsComponent,
-    CategoryListComponent
   ],
   imports: [
     BrowserModule,
