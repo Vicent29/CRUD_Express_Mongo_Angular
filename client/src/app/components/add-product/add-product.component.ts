c6ebdf2844bacd81e01f11f12eaff358cd9cb44a
Vicent Esteve <esteve.ferre.vicent@gmail.com>
2022-09-26 13:28:24 +0200

    Reactive form product finished

--- client/src/app/components/add-product/add-product.component.ts
+++ client/src/app/components/add-product/add-product.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
@@ -10,51 +12,50 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AddProductComponent implements OnInit {
 
-  product: Product = {
-    prod_nom: '',
-    id_prod_typ: '',
-    prod_desc: '',
-    precio: '',
-    id_prod_cat: ''
-  };
-  submitted = false;
+  form: FormGroup = new FormGroup({
+    prod_nom: new FormControl(''),
+    id_prod_typ: new FormControl(''),
+    prod_desc: new FormControl(''),
+    precio: new FormControl(''),
+    id_prod_cat: new FormControl(''),
+  });
+  firstSubmit: Boolean = true;
 
   constructor(
     private productService: ProductService,
-    private toastrService: ToastrService
+    private toastrService: ToastrService,
+    private formBuilder: FormBuilder,
+    private router: Router,
     ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        prod_nom: ['', [Validators.required, Validators.minLength(4)]],
+        id_prod_typ: ['', [Validators.required, Validators.minLength(4)]],
+        prod_desc: ['', [Validators.required, Validators.minLength(4)]],
+        precio: ['', [Validators.required, Validators.pattern("^[0-9]*$"),Validators.minLength(1)]],
+        id_prod_cat: ['', [Validators.required, Validators.minLength(4)]],
+      },
+    );
   }
 
-  saveProduct(): void {
-    const data = {
-      prod_nom: this.product.prod_nom,
-      id_prod_typ: this.product.id_prod_typ,
-      prod_desc: this.product.prod_desc,
-      precio: this.product.precio,
-      id_prod_cat: this.product.id_prod_cat
-    };
-
-    this.productService.create(data)
-      .subscribe({
-        next: (res) => {
-          this.toastrService.success('Added successfully!','The '+ data.id_prod_typ +' '+ data.prod_nom + ' added successfully!');
-          this.submitted = true;
-        },
-        error: (e) => console.error(e)
-      });
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
   }
 
-  newProduct(): void {
-    this.submitted = false;
-    this.product = {
-      prod_nom: '',
-      id_prod_typ: '',
-      prod_desc: '',
-      precio: '',
-      id_prod_cat: ''
-    };
+  addProduct(): void {
+    console.log(this.form.controls);
+    this.firstSubmit = false;
+    if (this.form.valid) {
+      this.productService.create(this.form.value)
+        .subscribe({
+          next: (res) => {
+            this.toastrService.success("This product has been aded");
+            this.router.navigate(['/product']);
+          },
+          error: (e) => this.toastrService.error("Can't add this product")
+        });
+    }
   }
-
 }
